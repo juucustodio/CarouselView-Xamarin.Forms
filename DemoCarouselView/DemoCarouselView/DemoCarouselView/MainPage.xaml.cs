8b2231cfe7ca664edb7989aedec5b709218ed6f9
Juliano Custódio <juliano.custodio@hotmail.com.br>
2017-05-12 14:00:27 -0300

    Implementing CarouselView

--- DemoCarouselView/DemoCarouselView/DemoCarouselView/MainPage.xaml.cs
+++ DemoCarouselView/DemoCarouselView/DemoCarouselView/MainPage.xaml.cs
@@ -12,6 +12,36 @@ namespace DemoCarouselView
         public MainPage()
         {
             InitializeComponent();
+
+            /*//Exemplo 1 - CarouselView com Texto
+            var listaTexto = new List<string>
+            {
+                "Texto 1",
+                "Texto 2",
+                "Texto 3"
+            };
+
+            TextoCarouselView.ItemsSource = listaTexto;*/
+
+            //Exemplo 2 - CarouselView com Imagem da Web
+            var listaLink = new List<string>
+            {
+                "http://migre.me/wB66p",
+                "http://migre.me/wB67d",
+                "http://migre.me/wB6gJ"
+            };
+
+            ImageCarouselView.ItemsSource = listaLink;
+
+            /*//Exemplo 3 - CarouselView com Imagem Local
+            var listaImagens = new List<string>
+            {
+                "foto1.jpg",
+                "foto2.jpg",
+                "foto3.jpg"
+            };
+
+            ImageCarouselView.ItemsSource = listaImagens;*/
         }
     }
 }
